2289fd6f7ab04b1260677a20f562999c1aa7ae6c
naveen965 <naveendevinda163@gmail.com>
2024-04-28 16:15:37 +0530

    Updated

--- src/components/navbar/Navbar.components.tsx
+++ src/components/navbar/Navbar.components.tsx
@@ -1,5 +1,9 @@
+import { useContext } from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
+import { Menu } from "@mui/icons-material";
+import { ToggleButton } from "@mui/material";
+import { ThemeContext } from "../../context/theme.context";
 
 const links = [
   { href: "/", label: "Home" },
@@ -9,14 +13,27 @@ const links = [
 ];
 
 const Navbar = () => {
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
   return (
     <div className="navbar">
       <div className="brand">
         <span>Resume Management</span>
       </div>
-      <div className="menu"></div>
-      <div className="hamburger"></div>
-      <div className="toggle"></div>
+      <div className="menu">
+        <ul>
+          {links.map((item) => (
+            <li key={item.href}>
+              <Link to={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="hamburger">
+        <Menu />
+      </div>
+      <div className="toggle">
+        <ToggleButton value={"check"} selected={darkMode} onChange={toggleDarkMode}></ToggleButton>
+      </div>
     </div>
   );
 };
