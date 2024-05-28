bdfbbc4c8cb4b3e5833b22980f8ce885bb01ced4
naveen965 <naveendevinda163@gmail.com>
2024-05-28 16:59:20 +0530

    edit companies.page.tsx

--- src/pages/companies/Companies.page.tsx
+++ src/pages/companies/Companies.page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import "./companies.scss";
+import httpModule from '../../helpers/http.module';
+import { ICompany } from '../../types/global.typing'; 
+import { Button } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import { redirect, useNavigate } from 'react-router-dom';
 
 const Companies = () => {
+  const [ companies, setCompanies ] = useState<ICompany[]>([])
+  const [ loading, setLoading ] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    httpModule.get<ICompany[]>("/Company/Get")
+    .then(response => {
+      setCompanies(response.data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      alert("Error");
+      console.log(error);
+      setLoading(false);
+    })
+  }, []);
+
+  console.log(companies);
+
   return (
-    <div>Companies</div>
+    <div className="content companies">
+      <div className="heading">
+        <h2>Companies</h2>
+        <Button variant='outlined' onClick={() => redirect("/companies/add")}>
+          <Add />
+        </Button>
+      </div>
+    </div>
   )
 }
 
